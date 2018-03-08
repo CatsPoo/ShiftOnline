bf4a2cb59ddb2f3ed40a22ddf160be1cb81fd707
unknown <u88044@lotem.il>
2018-03-08 10:22:34 +0200

    fix set last update for bullet

--- models/Bullets.js
+++ models/Bullets.js
@@ -1,58 +1,58 @@
-var mongoose=require('mongoose');
-var config=require('../config/database');
+var mongoose = require('mongoose');
+var config = require('../config/database');
 
-var bulletSchema=mongoose.Schema({
-    name:{
+var bulletSchema = mongoose.Schema({
+    name: {
         type: String,
         required: true
     },
-    dateOfCreation:{
+    dateOfCreation: {
         type: String,
         required: true
     },
-    lastUpdate:{
+    lastUpdate: {
         type: String,
         required: true
     },
-    color:{
+    color: {
         type: String,
         required: true
     },
-    content:{
+    content: {
         type: Object,
     }
 });
 
-module.exports=mongoose.model('Bullet',bulletSchema);
+module.exports = mongoose.model('Bullet', bulletSchema);
 
-module.exports.getBulletById=function(id,callback){
-    Bullet.findById(id,callback);
+module.exports.getBulletById = function(id, callback) {
+    Bullet.findById(id, callback);
 }
 
-module.exports.getAllBullets=function(callback){
+module.exports.getAllBullets = function(callback) {
     this.find(callback);
 }
 
-module.exports.addBullet=function(newBullet,callback){
+module.exports.addBullet = function(newBullet, callback) {
     newBullet.save(callback);
 }
 
-module.exports.removeBullet=function(bulletID,callback){
+module.exports.removeBullet = function(bulletID, callback) {
     console.log(bulletID);
     this.findById(bulletID).remove(callback);
 }
 
-module.exports.addNewContentToBullet=function(bulletID,content,callback){
-    this.findById(bulletID).update({_id:bulletID} ,{$push: {"content":content}},callback);
+module.exports.addNewContentToBullet = function(bulletID, content, callback) {
+    this.findById(bulletID).update({ _id: bulletID }, { $push: { "content": content } }, callback);
 }
 
-module.exports.updateTodayContentOfBullet=function(bulletID,content,callback){
+module.exports.updateTodayContentOfBullet = function(bulletID, content, callback) {
     console.log(content);
-   this.findById(bulletID).update({_id:bulletID} ,{$pop: {"content":1}},(err,res)=>{});
-   this.findById(bulletID).update({_id:bulletID} ,{$push: {"content":content}},callback);
+    this.findById(bulletID).update({ _id: bulletID }, { $pop: { "content": 1 } }, (err, res) => {});
+    this.findById(bulletID).update({ _id: bulletID }, { $push: { "content": content } }, callback);
 }
-module.exports.updateLastUpdate=function(bulletID,lastUpdate,callback){
-    let lastUpdateString=lastUpdate._day+"/"+lastUpdate._mounth+"/"+lastUpdate._year;
-   this.findById(bulletID).update({_id:bulletID} , {"lastUpdate":lastUpdateString},(err,res)=>{});
+module.exports.updateLastUpdate = function(bulletID, lastUpdate, callback) {
+    let lastUpdateString = lastUpdate._day + "/" + lastUpdate._mounth + "/" + lastUpdate._year;
+    this.findById(bulletID).update({ _id: bulletID }, { $set: { "lastUpdate": lastUpdateString } }, (err, res) => {});
 
 }
